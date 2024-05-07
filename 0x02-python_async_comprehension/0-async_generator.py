d79ff3619e55ca72dc4594f651f911e5576da78e
esmaelebrah <esmaelebrahimhasengmail.com>
2024-05-07 17:25:50 +0300

    updated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-import random
-import asyncio
+
+""" Async Comprehensions """
+
+from asyncio import sleep
+from random import uniform
 from typing import Generator
-async def async_generator() -> Generator[int, None, None]:
-    for i in range(10):
-        num = random.uniform(0, 10)
-        yield num
-        await asyncio.sleep(1)
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-import time
-import asyncio
+""" Async Comprehensions """
+
+from asyncio import gather
+from time import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime():
-    start_time = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
-    last_time = time.time()
-    return (last_time - start_time)
+
+async def measure_runtime() -> float:
+    """ Run time for four parallel comprehensions """
+    start = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await gather(*tasks)
+    end = time()
+    return (end - start)
